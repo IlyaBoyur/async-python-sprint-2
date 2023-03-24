81e8f086df1fa0ac7229a22423c6b647d166c394
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-24 15:54:03 +0300

    Add web job

---
+++ jobs/web_job.py
@@ -0,0 +1,33 @@
+import requests
+from .job import Job
+from typing import List
+import logging
+
+
+URLS_DEFAULT = [
+    "https://google.com/",
+    "https://ya.ru/",
+    "https://www.rambler.ru/",
+    "https://www.yahoo.com/",
+    "https://www.bing.com/",
+]
+
+
+logger = logger.getLogger(__name__)
+
+
+class WebJob(Job):
+    def __init__(self, urls:List[str]=URLS_DEFAULT):
+        super().__init__()
+        self.urls = urls
+
+    def target(self):
+        super().target()
+        try:
+            for url in self.urls:
+                response = requests.get(url)
+                response.raise_for_status()
+                yield response
+        except requests.exceptions.HTTPError as error:
+            logger.error(error)
+            self.retry()
