2a4670286b03c68e7c38fca86c95d6db870849c9
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-27 12:00:31 +0300

    Add WebJob test

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,12 +1,57 @@
 import pytest
-from jobs.job import WebJob
+from jobs import WebJob, URLS_DEFAULT
 from scheduler import Scheduler
+from queue import Queue
 
 
 class TestWebJob:
+    @pytest.mark.skip
     def test_default(self):
-        job = WebJob()
-        scheduler = Scheduler(3)
+        job = WebJob(max_working_time=3)
+        scheduler = Scheduler(pool_size=3)
         scheduler.run()
         scheduler.schedule(job)
         scheduler.join()
+
+    @pytest.mark.skip
+    def test_logging(self):
+        import logging
+
+        logging.basicConfig(
+            format="[%(levelname)s] - %(asctime)s - %(message)s",
+            level=logging.DEBUG,
+            datefmt="%H:%M:%S",
+        )
+        scheduler = Scheduler(pool_size=3)
+        scheduler.run()
+        job = WebJob(max_working_time=3)
+        scheduler.schedule(job)
+        scheduler.join()
+
+
+    def test_queue(self):
+        import json
+
+        CITIES = {
+            "MOSCOW": "https://code.s3.yandex.net/async-module/moscow-response.json",
+            "PARIS": "https://code.s3.yandex.net/async-module/paris-response.json",
+            "LONDON": "https://code.s3.yandex.net/async-module/london-response.json",
+        }
+        test_queue = Queue()
+        job = WebJob(urls=CITIES.values(), queue=test_queue)
+        scheduler = Scheduler(pool_size=3)
+        scheduler.run()
+        scheduler.schedule(job)
+        scheduler.join()
+
+        size = test_queue.qsize()
+        assert size == len(CITIES)
+        result = []
+        result.extend(json.loads(test_queue.get())  for i in range(len(CITIES)))
+            
+        for response in result:
+            assert "info" in response
+            assert "geo_object" in response
+            assert "fact" in response
+            assert "yesterday" in response
+            assert "forecasts" in response
