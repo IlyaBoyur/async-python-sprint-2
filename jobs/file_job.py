552d65dbe6df85b07386087612288ee162b12e9f
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-27 18:54:35 +0300

    Create file job

---
+++ jobs/file_job.py
@@ -0,0 +1,31 @@
+from .job import Job
+from typing import List, Tuple, Any
+import os
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class FileJob(Job):
+    def __init__(self, actions: List[Tuple[str, str, Any]], *args, **kwargs):
+        super().__init__()
+        self.actions = actions
+
+    def target(self):
+        for filemode, file, target in self.actions:
+            try:
+                if filemode == "w":
+                    with open(file=file, mode=filemode) as file:
+                        file.write(target)
+                elif filemode == "r":
+                    if not os.path.exists(file):
+                        raise RuntimeError(f"Невозможно прочитать файл {file}: файл отсутствует")
+                    with open(file=file, mode=filemode) as file:
+                        target[0] = file.read()
+                else:
+                    logger.warning("Режим не поддерживается")
+            except RuntimeError as error:
+                logger.error(error)
+            yield
+            
