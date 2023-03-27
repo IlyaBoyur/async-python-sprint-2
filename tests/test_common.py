2cd35b065edf488449cb60cdf8dd8e3324a64ca6
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-27 19:45:44 +0300

    Add common scheduler tests

---
+++ tests/test_common.py
@@ -0,0 +1,55 @@
+import pytest
+from jobs import InfiniteJob
+from scheduler import Scheduler
+from queue import Queue
+import json
+
+
+class TestSchedulerCommon:
+    @pytest.fixture
+    def prepare_lock(self):
+        active_jobs = [InfiniteJob() for _ in range(4)]
+        scheduler = Scheduler()
+        [scheduler.schedule(job) for job in active_jobs]
+        scheduler.stop()
+        return scheduler, active_jobs
+
+    def test_task_size(self, clear):
+        jobs = [InfiniteJob() for _ in range(11)]
+        scheduler = Scheduler()
+        [scheduler.schedule(job) for job in jobs]
+
+        assert len(scheduler.tasks_active) == 10
+        assert len(scheduler.tasks_wait) == 1
+
+    def test_stop(self, clear):
+        active = 5
+        waiting = 4
+        jobs_active = [InfiniteJob() for _ in range(active)]
+        jobs_waiting = [InfiniteJob() for _ in range(waiting)]
+        scheduler = Scheduler(pool_size=active)
+        scheduler.run()
+        [scheduler.schedule(job) for job in [*jobs_active, *jobs_waiting]]
+        scheduler.stop()
+
+        with open("scheduler.lock", "r") as file:
+            data = json.load(file)
+
+        assert len(data["active"]) == len(jobs_active)
+        assert len(data["waiting"]) == len(jobs_waiting)
+
+    def test_restart(self, clear, prepare_lock):
+        scheduler, active_jobs = prepare_lock
+        active_jobs_count = len(active_jobs)
+
+        scheduler.restart()
+        scheduler.pause()
+
+        assert len(scheduler.tasks_active) == active_jobs_count
+        assert len(scheduler.tasks_wait) == 0
+    
+    def test_singleton(self, clear):
+        scheduler = Scheduler()
+        scheduler_other = Scheduler()
+
+        assert id(scheduler) == id(scheduler_other)
