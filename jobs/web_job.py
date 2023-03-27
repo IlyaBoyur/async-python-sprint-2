50a59568bfdc3b7f530d73308e36d283e8a71671
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-27 12:00:00 +0300

    Add output for WebJob

--- jobs/web_job.py
+++ jobs/web_job.py
@@ -2,6 +2,7 @@ import requests
 from .job import Job
 from typing import List
 import logging
+from queue import Queue
 
 
 URLS_DEFAULT = [
@@ -13,21 +14,23 @@ URLS_DEFAULT = [
 ]
 
 
-logger = logger.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class WebJob(Job):
-    def __init__(self, urls:List[str]=URLS_DEFAULT):
-        super().__init__()
+    def __init__(self, urls:List[str]=URLS_DEFAULT, queue: Queue=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.urls = urls
+        self.queue = queue
 
     def target(self):
-        super().target()
         try:
             for url in self.urls:
                 response = requests.get(url)
                 response.raise_for_status()
                 logger.info(f"{response.status_code}: {response.content[:100]}")
+                if self.queue is not None:
+                    self.queue.put(response.content)
                 yield response
         except requests.exceptions.HTTPError as error:
             logger.error(error)
