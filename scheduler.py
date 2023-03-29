36e2fe8f6d3347390ca7fe7704f07b4fe810493b
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-29 22:28:04 +0300

    Hide internal methods in private members

--- scheduler.py
+++ scheduler.py
@@ -32,27 +32,26 @@ class Scheduler(metaclass=SingletonMeta):
         self.event_loop_started = False
 
     def schedule(self, task: Job):
-        self._stop_event_loop()
+        self.__stop_event_loop()
         if len(self.tasks_active) + len(task.dependencies) < self.pool_size:
             self.tasks_active.extend(task.dependencies)
             self.tasks_active.append(task)
         else:
             self.tasks_wait.extend(task.dependencies)
             self.tasks_wait.append(task)
-        self._start_event_loop()
+        self.__start_event_loop()
 
     def run(self):
-        self._start_event_loop()
+        self.__start_event_loop()
 
     def restart(self):
         # read task states
         with open(self.lockfile, "r") as file:
-            # waiting, active = pickle.load(file)
             data = json.load(file)
         active = data.get("active", [])
         waiting = data.get("waiting", [])
         # stop event loop
-        self._stop_event_loop()
+        self.__stop_event_loop()
         # restore waiting tasks
         for state in [*waiting, *active[self.pool_size :]]:
             job_type = state.get("job_type")
@@ -64,14 +63,14 @@ class Scheduler(metaclass=SingletonMeta):
             JobKlass = JOB_TYPES.get(job_type, Job)
             self.tasks_active.append(JobKlass(**state))
         # start event loop
-        self._start_event_loop()
+        self.__start_event_loop()
 
     def pause(self):
-        self._stop_event_loop()
+        self.__stop_event_loop()
 
     def stop(self):
         # stop event loop
-        self._stop_event_loop()
+        self.__stop_event_loop()
         # stop tasks
         for task in self.tasks_active:
             task.stop()
@@ -82,26 +81,31 @@ class Scheduler(metaclass=SingletonMeta):
         # write task states
         data = {"active": active, "waiting": waiting}
         with open(self.lockfile, "w") as file:
-            # pickle.dump(waiting, file)
-            # pickle.dump(active, file)
             json.dump(data, file)
         # remove tasks
         self.tasks_wait = []
         self.tasks_active = []
 
-    def _start_event_loop(self):
+    def join(self):
+        while True:
+            with self.lock:
+                if len(self.tasks_active) + len(self.tasks_wait) == 0:
+                    break
+            time.sleep(ITER_SECS)
+
+    def __start_event_loop(self):
         if not self.event_loop_started:
-            thread = Thread(target=self._event_loop, daemon=True)
+            thread = Thread(target=self.__event_loop, daemon=True)
             thread.start()
             self.event_loop_started = True
         if self.lock.locked():
             self.lock.release()
 
-    def _stop_event_loop(self):
+    def __stop_event_loop(self):
         if not self.lock.locked():
             self.lock.acquire()
 
-    def _event_loop(self):
+    def __event_loop(self):
         logger.info(f"event loop: started")
         current = 0
         while True:
@@ -115,7 +119,7 @@ class Scheduler(metaclass=SingletonMeta):
                 job = self.tasks_active[current]
                 if not job.is_finished:
                     logger.info(f"event loop: job %s iteration started", job)
-                    self._process_job(job)
+                    self.__process_job(job)
                     logger.info(f"event loop: job %s iteration finished", job)
                 else:
                     # 2) if job is done - remove job and add job from wait list
@@ -131,12 +135,5 @@ class Scheduler(metaclass=SingletonMeta):
                 current = (current + 1) % active if active > 0 else 0
 
     @staticmethod
-    def _process_job(job: Job):
+    def __process_job(job: Job):
         job.run()
-
-    def join(self):
-        while True:
-            with self.lock:
-                if len(self.tasks_active) + len(self.tasks_wait) == 0:
-                    break
-            time.sleep(ITER_SECS)
